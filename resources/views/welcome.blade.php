1d13ac8cc68df8c87e3dddb23d4982cf12f3477c
propaganisa115 <propaganisa.reg@gmail.com>
2021-04-18 22:26:24 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,316 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Iis Anisa - Backend Developer | Resume & Portofolio</title><link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'>
+<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css'>
+<link rel="stylesheet" href="{{asset('CSS/bootstrap-social.css')}}">
+<link rel="stylesheet" href="{{asset('css/style.css')}}">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<section class="hero">
+  <div class="content">
+    <div class="header">
+      <h1>IIS ANISA </h1><h1>BACKEND DEVELOPER</h1>
+      <h2>Here you'll find my resume & some of my recent works as well.<br>
+Have a look around and if you're interested in working together, get in touch!</h2><BR>
+
+<a href="https://github.com/propaganisa115" target="_blank">  <button type="button" class="btn btn-social btn-lg btn-github"><span class="fa fa-github"></span>Github</button></a>
+  <a href="https://www.linkedin.com/in/iis-anisa-52164b117/" target="_blank">  <button type="button" class="btn btn-lg btn-social btn-linkedin"><span class="fa fa-linkedin"></span>linkedin</button></a>
+
+    </div>
+
+  </div>
+</section>
+<section class="about-myself">
+  <div class="content">
+    <h2>Hi !</h2>
+    <p>I'm <span>Iis Anisa</span> Born & raised in Bandung, Indonesia. I'm a Backend Developer with extensive experience for over<span> 5 years</span> who still learning by time and doing. I was graduated from <span> Widyatama University</span> with major <span> Technical Information</span>. I was Graduated in <span>2020 with GPA 3.30</span>. My expertise is to create websites using several programming languages ​​such as <span>PHP, python, etc</span>. The frameworks I usually use are CodeIgniter and Laravel. The databases that I master are <span>MySql, PostgreSql, Oracle, MongoDB, SqlServer, and others.</span</p>
+  </div>
+</section>
+
+<section class="experience">
+  <div class="content">
+    <h1>Experience</h1>
+    <ul>
+      <li>
+        <div class="experience-content ">
+          <h2>PT INSAN INFONESIA – PROGRAMMER </h2>
+          <div class="experience-time">2015-2018</div>
+          <p>Developed application using PHP and MySQL ∙ Learning various things such as GIT, Phyton language,  Api, Wiki, Putty, security programming, penetration  testing.</p>
+        </div>
+      </li>
+      <li>
+        <div class="experience-content ">
+          <h2>CV RC ELECTRONICS – DESKTOP PROGRAMMER </h2>
+          <div class="experience-time">2014-2015</div>
+          <p> Developed stock application for company based factory  in Bandung and Jakarta using foxpro and MySQL
+database
+</p>
+        </div>
+      </li>
+      <li>
+        <div class="experience-content ">
+          <h2>BANK BPR NCP – IT STAFF </h2>
+          <div class="experience-time">2012-2013</div>
+          <p>Developed application using ms access. Managing company database using PostgreSQL. IT support job such as installing and configuring  computer, repair printer, etc.
+</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</section>
+<section class="skills">
+  <div class="content">
+    <div class="development-wrapper">
+      <h2 class="development-title">Development</h2>
+      <ul class="skills-bar-container">
+        <li>
+          <div class="progressbar-title">
+            <h3>HTML5</h3>
+            <span class="percent" id="html-pourcent"></span>
+          </div>
+          <div class="bar-container" data-percent="95">
+            <span class="progressbar" id="progress-html"></span>
+          </div>
+        </li>
+        <li>
+          <div class="progressbar-title">
+            <h3>PHP</h3>
+            <span class="percent" id="css-pourcent"></span>
+          </div>
+          <div class="bar-container" data-percent="85">
+            <span class="progressbar" id="progress-css"></span>
+          </div>
+        </li>
+        <li>
+          <div class="progressbar-title">
+            <h3>JavaScript</h3>
+            <span class="percent" id="javascript-pourcent"></span>
+          </div>
+          <div class="bar-container" data-percent="50">
+            <span class="progressbar" id="progress-javascript"></span>
+          </div>
+        </li>
+        <li>
+          <div class="progressbar-title">
+            <h3>Laravel</h3>
+            <span class="percent" id="php-pourcent"></span>
+          </div>
+          <div class="bar-container" data-percent="80">
+            <span class="progressbar" id="progress-php"></span>
+          </div>
+        </li>
+        <li>
+          <div class="progressbar-title">
+            <h3>CodeIgniter</h3>
+            <span class="percent" id="angular-pourcent"></span>
+          </div>
+          <div class="bar-container" data-percent="70">
+            <span class="progressbar" id="progress-angular"></span>
+          </div>
+        </li>
+        <li>
+          <div class="progressbar-title">
+            <h3>Django</h3>
+            <span class="percent" id="angular-pourcent"></span>
+          </div>
+          <div class="bar-container" data-percent="50">
+            <span class="progressbar" id="progress-angular"></span>
+          </div>
+        </li>
+        <li>
+          <div class="progressbar-title">
+            <h3>Python</h3>
+            <span class="percent" id="angular-pourcent"></span>
+          </div>
+          <div class="bar-container" data-percent="50">
+            <span class="progressbar" id="progress-angular"></span>
+          </div>
+        </li>
+        <li>
+          <div class="progressbar-title">
+            <h3>GIT</h3>
+            <span class="percent" id="angular-pourcent"></span>
+          </div>
+          <div class="bar-container" data-percent="70">
+            <span class="progressbar" id="progress-angular"></span>
+          </div>
+        </li>
+        <li>
+          <div class="progressbar-title">
+            <h3>MySQL</h3>
+            <span class="percent" id="angular-pourcent"></span>
+          </div>
+          <div class="bar-container" data-percent="80">
+            <span class="progressbar" id="progress-angular"></span>
+          </div>
+        </li>
+        <li>
+          <div class="progressbar-title">
+            <h3>MongoDB</h3>
+            <span class="percent" id="angular-pourcent"></span>
+          </div>
+          <div class="bar-container" data-percent="70">
+            <span class="progressbar" id="progress-angular"></span>
+          </div>
+          <li>
+            <div class="progressbar-title">
+              <h3>CodeIgniter</h3>
+              <span class="percent" id="angular-pourcent"></span>
+            </div>
+            <div class="bar-container" data-percent="75">
+              <span class="progressbar" id="progress-angular"></span>
+            </div>
+          </li>
+          <li>
+            <div class="progressbar-title">
+              <h3>API</h3>
+              <span class="percent" id="angular-pourcent"></span>
+            </div>
+            <div class="bar-container" data-percent="70">
+              <span class="progressbar" id="progress-angular"></span>
+            </div>
+          </li>
+          <li>
+            <div class="progressbar-title">
+              <h3>Linux</h3>
+              <span class="percent" id="angular-pourcent"></span>
+            </div>
+            <div class="bar-container" data-percent="70">
+              <span class="progressbar" id="progress-angular"></span>
+            </div>
+          </li>
+          <li>
+            <div class="progressbar-title">
+              <h3>English Languange(score EPT 557)</h3>
+              <span class="percent" id="angular-pourcent"></span>
+            </div>
+            <div class="bar-container" data-percent="80">
+              <span class="progressbar" id="progress-angular"></span>
+            </div>
+          </li>
+        </li>
+      </ul>
+    </div>
+    <div class="tools-knowledge-wrapper">
+      <div class="tools-wrapper">
+        <h2 class="title">Tools</h2>
+        <ul class="tools">
+          <li>
+            <i class="fa fa-check" aria-hidden="true"></i>
+            Atom
+          </li>
+          <li>
+            <i class="fa fa-check" aria-hidden="true"></i>
+            Postman
+          </li>
+          <li>
+            <i class="fa fa-check" aria-hidden="true"></i>
+            Sublime text
+          </li>
+          <li>
+            <i class="fa fa-check" aria-hidden="true"></i>
+            Putty
+          </li>
+        </ul>
+      </div>
+
+    </div>
+    <div class="clear"></div>
+  </div>
+  <div class="clear"></div>
+</section>
+<section class="resume">
+  <div class="content">
+    <a href="#" target="_blank">
+      <div class="btn-resume">
+        <i class="fa fa-download fa-2x" aria-hidden="true"></i>
+      <a href="{{asset('file/cv_iis.pdf')}}" download="CV_IIS_ANISA">  <span>Download my resume</span></a>
+      </div>
+    </a>
+  </div>
+</section>
+<section class="portfolio">
+  <div class="content">
+
+    <h1>Some works</h1>
+
+    <div class="projects">
+
+      <div class="project">
+        <div class="project-image">
+          <img src="https://i.ibb.co/3hyZHXQ/cv.png" alt=""/>
+        </div>
+        <div class="project-title">
+          <a href="">  <h2>CV website</h2></a>
+        </div>
+        <div class="project-description">
+          <p>Merupakan sebuah website berbasis PHP dan laravel yang berisi curriculum vitae. Berisi informasi tentang kualifikasi saya sebagai seorang backend developer. Informasi yang disebutkan dalam website tersebut mencakup data pribadi, latar belakang pendidikan, prestasi, keterampilan, pengalaman profesional, dan lain-lain </p>
+        </div>
+      </div>
+      <div class="project">
+        <div class="project-image">
+          <img src="https://i.ibb.co/4Shtwfv/ecommerce.png" alt=""/>
+        </div>
+        <div class="project-title">
+          <a href="">  <h2>Mini ecommerce website</h2></a>
+        </div>
+        <div class="project-description">
+          <p>Merupakan sebuah website berbasis PHP dan laravel hasil pembelajaran dari course laravel. Pada website tersebut terdapat modul autentikasi, shop, cart, kirim detail cart ke email dan lain lain. </p>
+        </div>
+      </div>
+      <div class="project">x
+        <div class="project-image">
+          <img src="https://i.ibb.co/Vt53kPC/weather.png" alt=""/>
+        </div>
+        <div class="project-title">
+          <h2>Weather Station</h2>
+        </div>
+        <div class="project-description">
+          <p>Personal weather station adalah perangkat sensor cuaca, yang akan memberikan informasi tentang cuaca di sekitar kota Bandung.
+          Pada tahap awal, terdapat sekitar 30 perangkat sensor cuaca yang ditempatkan di lokasi berbeda, sehingga diperlukan suatu aplikasi untuk dapat memantau, meng-agregat, dan memudahkan analisis hasil tangkapan sensor.
+          Aplikasi tersebut harus dapat diintegrasikan dengan aplikasi yang dimiliki oleh pemkot Kota Bandung</p>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</section>
+<section class="contact">
+  <div class="content">
+    <ul class="socials">
+      <li class="social">
+        <a href="#" target="_blank" class="email">
+          <i class="fa fa-envelope" aria-hidden="true"></i>
+        </a>
+      </li>
+      <li class="social">
+        <a href="#" target="_blank" class="twitter">
+          <i class="fa fa-twitter" aria-hidden="true"></i>
+        </a>
+      </li>
+      <li class="social">
+        <a href="#" target="_blank" class="linkedin">
+          <i class="fa fa-linkedin-square" aria-hidden="true"></i>
+        </a>
+      </li>
+      <li class="social">
+        <a href="#" target="_blank" class="codepen">
+          <i class="fa fa-codepen" aria-hidden="true"></i>
+        </a>
+      </li>
+    </ul>
+    <div class="clear"></div>
+  </div>
+</section>
+<section class="footer">
+  <p>Made with <i class="fa fa-heart" aria-hidden="true"></i> by IIS ANISA</p>
+</section>
+<!-- partial -->
+  <script src='https://code.jquery.com/jquery-2.2.4.min.js'></script><script  src="{{asset('js/script.js')}}"></script>
+
+</body>
+</html>
